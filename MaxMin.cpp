a708e4d62ccf77082ffd5934d43276c815b33b62
Suraj <surajyadav052005@gmail.com>
2024-09-22 21:42:06 +0530

    insertionsort

--- MaxMin.cpp
+++ MaxMin.cpp
@@ -30,6 +30,6 @@ int main(){
     for(int i = 0; i < size; i++){
         cin >> arr[i];
     }
-    cout << "max is : " << getMax(arr, size);
-      cout << "min is : " << getMin(arr, size);
-}
+    cout << "maximum is : " << getMax(arr, size);
+      cout << "minimum  is : " << getMin(arr, size);
+        }
