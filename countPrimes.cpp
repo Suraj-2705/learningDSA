55f52cc1da65440dc30a4e0d22f11bbeb9d5f110
Suraj <surajyadav052005@gmail.com>
2024-10-16 19:22:11 +0530

    Counting number of prime numbers stricltly less than n

---
+++ countPrimes.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+class Solution {
+public:
+    int countPrimes(int n) {
+        int cnt = 0;
+        vector<bool> prime(n+1, true);
+        prime[0] = prime[1] = false;
+        
+        for(int i = 0; i < n; i++){
+            if(prime[i]){
+                cnt++;
+
+                for(int j = 0; j < n; j=j+i){
+                    prime[j] = 0;
+                }
+            }
+        }
+
+        return cnt;
+        
+    }
+};
